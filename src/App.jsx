bae8eccd516a67b2f1f47b8aad5480d92a0d7af0
IWA1117 <chai22077@g-nihon.u.ac.jp>
2024-01-10 20:50:04 +0900

    変更

--- src/App.jsx
+++ src/App.jsx
@@ -199,7 +199,7 @@ export default function App() {
             {/* トレーニングメニュー */}
             <div id="trainingMenu">{generateMenuHTML()}</div>
             <h2>
-  全てチェックできたら<a href="../index2.html">こちら</a>をクリックしてください。
+  全てチェックできたら<a href="https://stately-cucurucho-e2126a.netlify.app/">こちら</a>をクリックしてください。
   </h2>
         </div>
     );
